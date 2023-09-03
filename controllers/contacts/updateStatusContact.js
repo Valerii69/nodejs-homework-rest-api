5c82d713587f50782c361a65789ec0ec4ccf70bc
Valerii Viun <2455521@gmail.com>
2023-09-03 14:43:36 +0300

    added

--- controllers/contacts/updateStatusContact.js
+++ controllers/contacts/updateStatusContact.js
@@ -1,14 +1,26 @@
 const { Contact } = require("../../models/contact");
-
 const { HttpError } = require("../../helpers");
+const { schemas } = require("../../models/contact");
 
-const updateStatusContact = async (req, res) => {
-  const { contactId } = req.params;
-  const result = await Contact.findByIdAndUpdate(contactId, req.body, { new: true });
-  if (!result) {
-    throw HttpError(404);
+const updateStatusContact = async (req, res, next) => {
+  try {
+    const { error } = schemas.updateFavoriteSchema.validate(req.body);
+    if (error) {
+      error.status = 400;
+      throw error;
+    }
+
+    const { contactId } = req.params;
+    const result = await Contact.findByIdAndUpdate(contactId, req.body, {
+      new: true,
+    });
+    if (!result) {
+      throw HttpError(404, "Not Found");
+    }
+    res.json(result);
+  } catch (error) {
+    next(error);
   }
-  res.json(result);
 };
 
 module.exports = updateStatusContact;
