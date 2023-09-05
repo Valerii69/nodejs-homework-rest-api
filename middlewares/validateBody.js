503dc054996c495b3dd28c5d87467b32fae57df6
Valerii Viun <2455521@gmail.com>
2023-09-05 15:05:25 +0300

    Update validateBody.js

--- middlewares/validateBody.js
+++ middlewares/validateBody.js
@@ -1,9 +1,15 @@
+const { HttpError } = require("../helpers");
+
 const validateBody = (schema) => {
   const func = (req, res, next) => {
     const keys = Object.keys(req.body);
-    if (keys.length === 0) {
+    if (req.method === "PUT" && keys.length === 0) {
       return res.status(400).json({ message: "Missing fields" });
     }
+    if (req.method === "PATCH" && keys.length === 0) {
+      return res.status(400).json({ message: "Missing field favorite" });
+    }
+
     const { error } = schema.validate(req.body);
     if (error) {
       next(HttpError(400, error.message));
