a942005b61b35fd9f106facfb433ef503640d6b2
Valerii Viun <2455521@gmail.com>
2023-09-08 23:03:58 +0300

    Update getCurrentUser.js

--- controllers/users/getCurrentUser.js
+++ controllers/users/getCurrentUser.js
@@ -1,12 +1,19 @@
 // const { HttpError } = require("../../helpers");
+const { User } = require("../../models/user");
 
-const getCurrentUser = (req, res) => {
-  const { email, subscription } = req.body;
-  // console.log(email, subscription);
-  res.json({
-    email,
-    subscription,
-  });
+const getCurrentUser = async (req, res, next) => {
+  try {
+    const user = await User.findOne(req.token);
+    const { email, subscription } = user;
+    user.password = undefined;
+
+    res.status(200).json({
+      email: email,
+      subscription: subscription,
+    });
+  } catch (error) {
+    next(error);
+  }
 };
 
 module.exports = getCurrentUser;
