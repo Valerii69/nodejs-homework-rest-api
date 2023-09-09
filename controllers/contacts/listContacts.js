568143602ea19923c99ab6ccc66653fc68d4039b
Valerii Viun <2455521@gmail.com>
2023-09-09 11:29:22 +0300

    Update listContacts.js

--- controllers/contacts/listContacts.js
+++ controllers/contacts/listContacts.js
@@ -2,23 +2,21 @@ const { Contact } = require("../../models/contact");
 
 const listContacts = async (req, res) => {
   const { _id: owner } = req.user;
-  const { favorite } = req.query;
-  const { page = 1, limit = 10 } = req.query;
+  const { page = 1, limit = 20, favorite = null } = req.query;
   const skip = (page - 1) * limit;
+  const query = { owner };
 
-  const query = favorite ? { owner, favorite: true } : {};
-  const options = { skip, limit };
-
-  try {
-    const result = await Contact.find(
-      query,
-      "-createdAt -updatedAt",
-      options
-    ).populate("owner", "email");
-    res.json(result);
-  } catch (error) {
-    console.error(error);
-    res.status(500).json({ error });
+  if (favorite !== null) {
+    query.favorite = favorite;
+    console.log(query);
   }
+
+  const result = await Contact.find(query, "-createdAt -updatedAt", {
+    skip,
+    limit,
+  }).populate("owner", "email");
+
+  res.json(result);
 };
+
 module.exports = listContacts;
