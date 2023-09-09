5c8fff95d17140cfc5ef121250359d0ad4b52c8c
Valerii Viun <2455521@gmail.com>
2023-09-09 11:23:32 +0300

    Update listContacts.js

--- controllers/contacts/listContacts.js
+++ controllers/contacts/listContacts.js
@@ -19,20 +19,4 @@ const listContacts = async (req, res) => {
   res.json(result);
 };
 
-// const { id } = req.user.id;
-// const { page = 1, limit = 10, favorite } = req.query;
-// const filterValue = favorite
-//   ? { ownerId: req.user.id, favorite }
-//   : { owner: id };
-
-// const skip = (page - 1) * limit;
-
-// // const options = { skip, limit };
-
-// const result = await Contact.find(filterValue, "", {
-//   skip,
-//   limit,
-// }).populate("owner", "email subscription");
-// res.json(result);
-// };
 module.exports = listContacts;
